57b172fa299b58dc78e137c2592999187f7f4bfc
montch <montch@gmail.com>
2016-08-29 13:15:02 -0400

    even more cleaner

--- index.ios.js
+++ index.ios.js
@@ -45,55 +45,52 @@ class Project extends Component {
         //ActivityToggle
         //TimePicker
         //AddButton
-    return (
-      <View>
-        <TimePicker timer={this.state.timer} setTotalTime={this.setTotalTime} setTimer={(val)=>this.setTimer(val)}/>
-        <AddButton time_total={this.state.time_total} setTotalTime={()=>this.setTotalTime()} />
-        <TotalTime  time_total={this.state.time_total} />
-        <ActivityToggle activity={this.state.activity}  status={this.state.status} setTotalTime={this.setTotalTime} />
-     
-      </View>
-    )
+      return (
+        <View>
+          <TimePicker timer={this.state.timer} setTotalTime={this.setTotalTime} setTimer={(val)=>this.setTimer(val)}/>
+          <AddButton time_total={this.state.time_total} setTotalTime={()=>this.setTotalTime()} />
+          <TotalTime  time_total={this.state.time_total} />
+          <ActivityToggle activity={this.state.activity}  status={this.state.status} setTotalTime={this.setTotalTime} />
+        </View>
+      )
     }
 }
   
 
  class TotalTime extends Component {
-     constructor(){
+   constructor(){
       super();
    }  
-      render() {
+    render() {
       return (
         <Text >    {this.props.time_total}  </Text>
       )
-      }
-   
+    }
  }
 
 
  class AddButton extends Component {
     constructor(){
       super();
-   }  
+    }  
    
    setNativeProps(nativeProps) {
     this._root.setNativeProps(nativeProps);
-  }
+   }
    
-     addTime(){
-      this.props.setTotalTime()
-     }
+   addTime(){
+    this.props.setTotalTime()
+   }
    
    render() {
      return (
        <View ref={component => this._root = component} {...this.props} >
          <TouchableHighlight onPress={()=>this.addTime()}>
-            <Text> Add </Text>
+            <Text> Add Time </Text>
           </TouchableHighlight>
        </View>
      )
    }
-   
  }
 
 
@@ -105,75 +102,71 @@ class Project extends Component {
       }
    }  
     render() {
-    return (
-      <Picker 
-        style={styles.picker}
-        selectedValue= { this.state.timer }
-        onValueChange={(value) => {
-          this.setState({timer: value});
-          this.props.setTimer({value});
-        }}>
-         <Picker.Item label={'1'} value={'1'} />
-         <Picker.Item label={'2'} value={'2'} />
-         <Picker.Item label={'3'} value={'3'} />
-         <Picker.Item label={'5'} value={'5'} />
-         <Picker.Item label={'8'} value={'8'} />
-         <Picker.Item label={'13'} value={'13'} />
-         <Picker.Item label={'21'} value={'21'} />
-         <Picker.Item label={'34'} value={'34'} />
-         <Picker.Item label={'55'} value={'55'} />
-      </Picker>
-      ) }
+      return (
+        <Picker 
+          style={styles.picker}
+          selectedValue= { this.state.timer }
+          onValueChange={(value) => {
+            this.setState({timer: value});
+            this.props.setTimer({value});
+          }}>
+           <Picker.Item label={'1'} value={'1'} />
+           <Picker.Item label={'2'} value={'2'} />
+           <Picker.Item label={'3'} value={'3'} />
+           <Picker.Item label={'5'} value={'5'} />
+           <Picker.Item label={'8'} value={'8'} />
+           <Picker.Item label={'13'} value={'13'} />
+           <Picker.Item label={'21'} value={'21'} />
+           <Picker.Item label={'34'} value={'34'} />
+           <Picker.Item label={'55'} value={'55'} />
+        </Picker>
+      )
+    }
   }
   
   class ActivityToggle extends Component {
-   constructor(){
-    super();
+    constructor(){
+      super();
       this.state ={
-       status: false, //this.props.status,
-       activity: 'No activity selected' //this.props.activity
+        status: false, //this.props.status,
+        activity: 'No activity selected' //this.props.activity
       }
-  }   
+    }   
     
-    
-  toggleStatus(){
-    this.setState({
-      status:!this.state.status
-    });
-  }
-  
- 
-    
-    render() {
-    return (
-    
-     <View style={styles.container}>
-       {renderIf(this.state.status)(
-        <View style={styles.container}>
-          <TextInput
-            style={styles.text_input}
-            placeholder={ this.state.activity }
-            placeholderTextColor={"rgba(198,198,204,1)"}
-            onChangeText={(activity) => { this.setState({activity:  activity} ) }}
-            onFocus={() => { this.setState({activity: ''})} }
-            onSubmitEditing={() => {this.setState({activity: ''})}}
-            value={(this.state && this.state.activity) || ''}
-  
-          />
-          <TouchableHighlight onPress={()=>this.toggleStatus()}>
-            <Text> Set </Text>
-          </TouchableHighlight>
-        </View>
-       )}
-        {renderIf(!this.state.status)(
-          <TouchableHighlight onPress={()=>this.toggleStatus()}>
-            <Text> { this.state.activity } </Text>
-          </TouchableHighlight>
-        )}
-      </View>
-    )
+    toggleStatus(){
+      this.setState({
+        status:!this.state.status
+      });
     }
+
+    render() {
+      return (
     
+       <View style={styles.container}>
+         {renderIf(this.state.status)(
+          <View style={styles.container}>
+            <TextInput
+              style={styles.text_input}
+              placeholder={ this.state.activity }
+              placeholderTextColor={"rgba(198,198,204,1)"}
+              onChangeText={(activity) => { this.setState({activity:  activity} ) }}
+              onFocus={() => { this.setState({activity: ''})} }
+              onSubmitEditing={() => {this.setState({activity: ''})}}
+              value={(this.state && this.state.activity) || ''}
+            />
+            <TouchableHighlight onPress={()=>this.toggleStatus()}>
+              <Text> Set </Text>
+            </TouchableHighlight>
+          </View>
+         )}
+          {renderIf(!this.state.status)(
+            <TouchableHighlight onPress={()=>this.toggleStatus()}>
+              <Text> { this.state.activity } </Text>
+            </TouchableHighlight>
+          )}
+        </View>
+      )
+    }
   }
   
   
